52fa92f7545e13c1eed2617779afd378a5268e24
vane2810 <olgavanessasf@gmail.com>
2022-11-25 19:22:25 -0600

    Agregar archivos de proyecto.

---
+++ Semana-16/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Semana_16.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bienvenida(string name, int numTimes = 1)
+        {
+            ViewData["name"] = "Hola " + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+
+        public string Parameters(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, su numero de intentos es: {numTimes}");
+        }
+    }
+}
---
+++ Semana-16/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Semana_16.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
