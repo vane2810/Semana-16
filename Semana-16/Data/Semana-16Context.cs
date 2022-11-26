9165f4447dec72dd26cd4ae457404992feea8c8d
vane2810 <olgavanessasf@gmail.com>
2022-11-25 19:51:27 -0600

    Agregar archivos de proyecto.

---
+++ Semana-16/Data/Semana-16Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Semana_16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana_16.Data
+{
+    public class Semana_16Context : DbContext
+    {
+        public Semana_16Context(DbContextOptions<Semana_16Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+    }
+}
